94ca45dcca4f4cd9619c4302879192812936907a
Farzad <farzad@Farzads-MacBook-Air.local>
2023-03-02 15:21:43 -0500

    c10 introduction au template-parts

--- front-page.php
+++ front-page.php
@@ -12,16 +12,10 @@
     <section class="blocflex">
     <?php 
     if (have_posts()):
-        while( have_posts()) : the_post();
-            //the_title('<h1>','</h1>');
-            //the_permalink();?>
-            <article>
-                <h3><a href="<?php the_permalink(); ?>"><?= get_the_title() ?></a></h3>
-                <p><?php echo wp_trim_words(get_the_excerpt(), 4);?></p>
-            </article>
-            <?php endwhile;?>
-            <?php endif;?>
+        while( have_posts()) : the_post(); ?>
+            <?php get_template_part("template_parts/categorie", "note-wp");?>
+        <?php endwhile;?>
+    <?php endif;?>
 </section>
-
 </main>
 <?php get_footer(); ?>
