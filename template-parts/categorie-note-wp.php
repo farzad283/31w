ac247d02a62b35f46a48d39984d1c10c5982cebe
Farzad <farzad@Farzads-MacBook-Air.local>
2023-04-09 14:43:57 -0400

    c19 Améliorer le modèle category-cours pour synthétiser l'offre de cours

--- template-parts/categorie-note-wp.php
+++ template-parts/categorie-note-wp.php
@@ -6,9 +6,14 @@
  * pour un article de catégorie note-wp
  */
 $titre = get_the_title();
-//if (substr($titre,0,1) == "0"){}
+$sigle = substr($titre, 0, 7);
+$titre_long = substr($titre, 7, -5);
 ?>
 <article class="blocflex__article">
-    <h5><a href="<?php the_permalink(); ?>"> <?= $titre; ?></a></h5>
+    <h5><a href="<?php the_permalink(); ?>"> <?= $sigle; ?></a></h5>
+    <h6><?= $titre_long ?></h6>
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+    <p><?php the_field('contenu');?></p>
+    <p><?php the_field('date');?></p>
+   
 </article>
