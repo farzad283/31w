d9ab5ad56700df6f9c64e0d6d04a6b25ee69c37c
Farzad <farzad@Farzads-MacBook-Air.local>
2023-04-14 13:05:34 -0400

    c19 ajouter une image en vedette à votre publication ou page unique en utilisant la fonction the_post_thumbnail().

--- single.php
+++ single.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
     Modèle index.php représente le modèle par défaut du thème
@@ -6,14 +5,14 @@
 get_header() ?>
 
 <main class="site__main">
-    <h3>single.php</h3>
-<?php 
-if (have_posts()):
-   while(have_posts()): the_post();
-        the_title('<h1>','</h1>');
-        the_content();  
-    endwhile;    
-endif;
-?>   
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="post-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        <?php endif; ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    <?php endwhile; endif; ?>   
 </main> 
 <?php get_footer(); ?>
