9d85d11217d5cb39d37f0261246182a334b21b48
Farzad <farzad@Farzads-MacBook-Air.local>
2023-04-18 14:36:50 -0400

    ajouter rechercher et text

--- 404.php
+++ 404.php
@@ -6,8 +6,14 @@
 
 <?php get_header()?>
 <main class="site__main">
-    <code>404.php</code>
-    <h1>Erreur 404 - La page n'existe pas</h1>
+    <!-- <code>404.php</code> -->
+    <h1>Erreur 404 </h1>
+    <h4>Page introuvable, vous pouvez tenter une recherche</h4>
+
+    <?php get_search_form( 'searchform' ); ?>
+    <label for="chkBurger" class="burger">
+        <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+        </label> 
 
 </main>
 <?php get_footer(); ?>
